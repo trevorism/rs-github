3f37a778bd82eda72c6f90b995da9bc56a585108
trevorism <trevorism@gmail.com>
2021-06-03 15:26:24 -0400

    Initial checkin of app shell

---
+++ src/app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <h1 class="title">ResultStack Github Search</h1>
+      </div>
+    </section>
+    <section class="section">
+      <Grid></Grid>
+    </section>
+
+  </div>
+</template>
+
+<script>
+import Grid from './components/Grid.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Grid
+  }
+}
+</script>
+
+<style>
+body::-webkit-scrollbar {
+  display: none;
+}
+</style>
---
+++ src/app/src/components/Grid.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div v-if="!loaded">
+      {{message}}
+    </div>
+    <div v-else-if="loaded">
+      <div v-for="topic in data" v-bind:key="topic.id">
+        {{topic}} <br/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Grid',
+  mounted () {
+    this.data = ['hi', 'bye']
+  },
+  data () {
+    return {
+      data: [],
+      loaded: false,
+      message: 'Loading...'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
