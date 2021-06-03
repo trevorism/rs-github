d670b21bb75a4bec295c959fdad9866bd2d6a6b0
trevorism <trevorism@gmail.com>
2021-06-03 16:07:08 -0400

    Added a Submit and clear Results button

--- src/app/src/App.vue
+++ src/app/src/App.vue
@@ -6,6 +6,22 @@
         <h1 class="title">ResultStack Github Search</h1>
       </div>
     </section>
+
+    <section class="section">
+      <b-field label="Search github by username or email">
+        <b-input :value="searchBarValue" class="m-3">
+        </b-input>
+        <button class="button is-info m-3" :disabled="disabled" @click="invokeButton">
+          Submit
+          <b-loading :is-full-page="false" :active.sync="disabled" :can-cancel="false"></b-loading>
+        </button>
+        <button class="button is-danger m-3" :disabled="disabled" @click="invokeButton">
+          Clear Results
+          <b-loading :is-full-page="false" :active.sync="disabled" :can-cancel="false"></b-loading>
+        </button>
+      </b-field>
+    </section>
+
     <section class="section">
       <Grid></Grid>
     </section>
@@ -20,7 +36,18 @@ export default {
   name: 'App',
   components: {
     Grid
+  },
+  data () {
+    return {
+      searchBarValue: 'hi'
+    }
+  },
+  methods: {
+    invokeButton: function () {
+      this.searchBarValue = 'bye'
+    }
   }
+
 }
 </script>
 
